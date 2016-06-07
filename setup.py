0a111da02530453631f31a23f04855d1b73c0659
Ryan.K <ryankung@ieee.org>
2016-06-07 15:46:15 +0800

    update setupfile and version

--- setup.py
+++ setup.py
@@ -2,39 +2,42 @@
 import sys
 from setuptools import setup, find_packages
 import os
+import codecs
+import re
 
-current_path = os.path.dirname(os.path.abspath(__file__))
-
-requirement_file = os.path.join(current_path, 'requirements.txt')
-version_file = os.path.join(current_path, 'VERSION')
+here = os.path.dirname(os.path.abspath(__file__))
 
 if sys.version_info[:2] < (3, 5) and sys.argv[-1] == 'install':
     sys.exit('stack requires python 3.5 or higher')
 
-with open(requirement_file, 'r') as f:
-    requires = [x.strip() for x in f if x.strip()]
 
-with open(version_file, 'r') as f:
-    version = f.read()
+def read(*parts):
+    # intentionally *not* adding an encoding option to open, See:
+    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    return codecs.open(os.path.join(here, *parts), 'r').read()
 
-with open(os.path.join(current_path, 'README.rst'), 'r') as f:
-    readme = f.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 
 setup(
-    name='python-stack',
-    version=version,
+    name='stack-scaffold',
+    version=find_version('__init__.py'),
     url='http://python-stack.readthedocs.io',
     description='`stack` is a Python version of [stack](http://docs.haskellstack.org/en/stable/README/),',
     author='Ryan Kung',
     py_modules=find_packages(exclude=['tests']),
     author_email='ryankung@ieee.org',
     license='MIT',
-    long_description=readme,
     packages=find_packages(exclude=['tests', 'docs']),
     package_dir={'': '.'},
     tests_require=['nose'],
-    install_requires=requires,
     test_suite='nose.collector',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -46,12 +49,7 @@ setup(
     ],
     include_package_data=True,
     entry_points={'console_scripts': [
-        'stack = stack.main:main',
         'scaffold = scaffold.main:main',
-        'pystack = stack.main:main',
-        'require = require.main:main',
-        'stack_init = stack.stack_init:main',
-        'pystack_init = stack.stack_init:main'
     ]},
 
 )
